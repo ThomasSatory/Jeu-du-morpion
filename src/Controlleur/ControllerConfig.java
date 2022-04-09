37ca6ecefb536264965517e3eaefff2d74d5eb82
ThomasSatory <thomas.satory@alumni.univ-avignon.fr>
2022-04-09 11:04:59 +0200

    onChanger working and add notif label

--- src/Controlleur/ControllerConfig.java
+++ src/Controlleur/ControllerConfig.java
@@ -2,8 +2,11 @@ package Controlleur;
 
 import Vue.ViewModeleConfig;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
 
 import javax.swing.*;
 import javax.swing.text.Element;
@@ -43,32 +46,47 @@ public class ControllerConfig {
     @FXML
     private RadioButton Difficile = new RadioButton("Difficile");
 
-    public ControllerConfig() throws IOException {
-    }
+    @FXML
+    private Label  Notif = new Label();
 
     @FXML
     protected void onChanger() throws IOException {
         String htext= h.getText();
         String ltext= l.getText();
         String lrtext=lr.getText();
+
+        Path path = Paths.get("resources/config.txt");
+        List<String> lines = Files.readAllLines(path);
+
         if(htext.length()!=0 && ltext.length()!=0 && lrtext.length()!=0){
+
             int hvalue = Integer.parseInt(htext);
             int lvalue = Integer.parseInt(ltext);
             float lrvalue = Float.parseFloat(lrtext);
-        }
-        Path path = Paths.get("resources/config.txt");
-        List<String> lines = Files.readAllLines(path);
-        if (Facile.isSelected()){
-            lines.add(0,"F:"+h+":"+lr+":"+l+" ");
-            lines.remove(0);
-        }
-        else if (Moyen.isSelected()){
-            lines.add(1,"M:"+h+":"+lr+":"+l+" ");
-            lines.remove(1);
-        }
-        else if (Difficile.isSelected()){
-            lines.add(2,"D:"+h+":"+lr+":"+l+" ");
-            lines.remove(2);
+
+            if (Facile.isSelected()){
+                lines.add(0,"F:"+hvalue+":"+lrvalue+":"+lvalue+" ");
+                lines.remove(1);
+            }
+
+            else if (Moyen.isSelected()){
+                lines.add(1,"M:"+hvalue+":"+lrvalue+":"+lvalue+" ");
+                lines.remove(2);
+            }
+
+            else if (Difficile.isSelected()){
+                lines.add(2,"D:"+hvalue+":"+lrvalue+":"+lvalue+" ");
+                lines.remove(3);
+            }
+
+            else{
+                Notif.setVisible(true);
+                Notif.setText("Veuillez choisir une difficulté");
+            }
+
+        } else {
+            Notif.setVisible(true);
+            Notif.setText("Veuillez renseigner tout les champs");
         }
 
         File file = new File("resources/config.txt");
@@ -76,20 +94,27 @@ public class ControllerConfig {
         if (!file.exists()) {
             file.createNewFile();
         }
+
         FileWriter fw = new FileWriter(file.getAbsoluteFile());
         BufferedWriter bw = new BufferedWriter(fw);
+
         for (int i =0 ;i< lines.size() ; i++){
             bw.write(lines.get(i));
-            bw.write("\r");
+            bw.newLine();
+        }
+        if((htext.length()!=0 && ltext.length()!=0 && lrtext.length()!=0) && (Facile.isSelected() || Moyen.isSelected() || Difficile.isSelected())){
+            Notif.setVisible(true);
+            Notif.setTextFill(Color.GREEN);
+            Notif.setText("Changement effectué");
         }
         bw.close();
-
     }
 
     protected ListView onStart() throws IOException {
         Path path = Paths.get("resources/config.txt");
         List<String> lines = Files.readAllLines(path);
-        ListView Liste = new ListView((Element) lines);
+        ObservableList<String> linesbis = FXCollections.observableArrayList(lines);
+        ListView Liste = new ListView((Element) linesbis);
         return Liste ;
     }
 
--- src/Vue/ViewModeleConfig.java
+++ src/Vue/ViewModeleConfig.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,8 @@ public class ViewModeleConfig extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(ViewModeleConfig.class.getResource("../fxmls/config.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         stage.setTitle("Configuration du jeu");
+        Label Notif = (Label) scene.lookup("#Notif");
+        Notif.setVisible(false);
         stage.setScene(scene);
         stage.show();
     }
